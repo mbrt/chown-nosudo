68079a15138b97dc3702e0de13b7737895768601
micheleb <michele.bertasi@brightcomputing.com>
2016-11-11 11:57:10 +0100

    check expected owner before change it
    another mitigation, that prevents to change unexpected ownerships

--- Makefile
+++ Makefile
@@ -5,11 +5,15 @@ SRCS=chown-nosudo.c
 chown-nosudo: $(SRCS)
 	$(CC) $(CFLAGS) -o $@ $^
 
-.PHONY: clean add-suid
+.PHONY: clean add-cap add-suid
 
 clean:
 	rm -f chown-nosudo
 
-add-suid:
+add-cap: chown-nosudo
+	sudo chown root:root chown-nosudo
+	sudo setcap cap_chown+ep chown-nosudo
+
+add-suid: chown-nosudo
 	sudo chown root:root chown-nosudo
 	sudo chmod 4755 chown-nosudo
--- chown-nosudo.c
+++ chown-nosudo.c
@@ -7,7 +7,7 @@
 // or:
 //
 // $ sudo chown root chown-nosudo
-// $ sudo setcap cap_chown+ep chown-nsudo
+// $ sudo setcap cap_chown+ep chown-nosudo
 //
 // (you can use cap_chown-ep to remove it afterwards).
 //
@@ -23,6 +23,8 @@
 
 #define DEBUG
 #define DIRECTORY "/home/micheleb/Downloads"
+#define OLD_UID 1170
+#define OLD_GID 1000
 #define NEW_UID 1000
 #define NEW_GID 1000
 
@@ -41,7 +43,7 @@ const int BLACKLIST_STAT[] = {
 };
 const unsigned int BLACKLIST_STAT_SIZE = sizeof(BLACKLIST_STAT) / sizeof(int);
 
-typedef void(*iterate_fn)(const char*);
+typedef int(*iterate_fn)(const char*, const struct stat* st);
 
 
 int is_allowed(const struct stat* st) {
@@ -91,7 +93,9 @@ int iterate_dir(const char* path, iterate_fn fn) {
             continue;
         }
         // apply the operation
-        fn(pathbuf);
+        if (!fn(pathbuf, &stbuf)) {
+            continue;
+        }
         // recurse if it's a subdir
         if ((stbuf.st_mode & S_IFMT) == S_IFDIR) {
             iterate_dir(pathbuf, fn);
@@ -104,15 +108,22 @@ cleanup:
     return result;
 }
 
-void print_path(const char* p) {
+int print_path(const char* p, const struct stat* st) {
     LOG("%s\n", p);
+    return 1;
 }
 
-void chown_path(const char* p) {
+int chown_path(const char* p, const struct stat* st) {
     int r;
+    if ((st->st_uid != OLD_UID && st->st_uid != NEW_UID) ||
+            (st->st_gid != OLD_GID && st->st_gid != NEW_GID)) {
+        LOG("%s: unexpected uid = %#o, or gid = %#o\n", p, st->st_uid, st->st_gid);
+        return 0;
+    }
     if (r = chown(p, NEW_UID, NEW_GID)) {
         LOG("%s: chown failed with errno %d\n", p, r);
     }
+    return 1;
 }
 
 int main() {
